c3b3915e235988df6ff5637faab8ce26f23cc9fa
guilhermedeoliveira <contatoguilhermedeoliveira@gmail.com>
2020-09-18 15:15:47 -0300

    feat: add basic pages

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,32 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import React from "react"
+import Link from "next/link"
+import styled from "styled-components"
+
+import Header from "styleguide/components/Header"
+
+const Main = styled.main`
+  text-align: center;
+  padding: 0 1rem;
+  font-size: 22px;
+`
+
+const Anchor = styled.a`
+  cursor: pointer;
+  color: ${({ theme }) => theme.colors.primary};
+`
 
 const Home = () => (
   <div>
-    <h1>Home page</h1>
+    <Header>Home</Header>
+
+    <Main>
+      <p>Do you want to take a picture?</p>
+
+      <Link href="/picture">
+        <Anchor>Go take a picture</Anchor>
+      </Link>
+    </Main>
   </div>
-);
+)
+
+export default Home
---
+++ styleguide/components/Header/index.js
@@ -0,0 +1,42 @@
+import React from "react"
+import { useRouter } from "next/router"
+import styled from "styled-components"
+import { BiArrowBack } from "react-icons/bi"
+
+const Wrapper = styled.header`
+  position: relative;
+  margin-bottom: 2rem;
+`
+
+const BackWrapper = styled.span`
+  position: absolute;
+  display: flex;
+  cursor: pointer;
+  padding: 1rem;
+`
+
+const StyledIcon = styled(BiArrowBack)`
+  fill: ${({ theme }) => theme.colors.primary};
+`
+
+const Title = styled.h1`
+  font-size: 50px;
+  text-align: center;
+  color: ${({ theme }) => theme.colors.primary};
+`
+
+const Header = ({ children }) => {
+  const { back } = useRouter()
+
+  return (
+    <Wrapper>
+      <BackWrapper onClick={back} style={{ border: "1px solid red" }}>
+        <StyledIcon size="2em" />
+      </BackWrapper>
+
+      <Title>{children}</Title>
+    </Wrapper>
+  )
+}
+
+export default Header
